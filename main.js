232ba899237c636833e3b60996f5ee662cd1e4ea
yechanyun <yyc837021@gmail.com>
2020-07-23 19:43:46 +0900

    init

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+     });
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    $('.top-nav .icon-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1200,
+    })
+});
